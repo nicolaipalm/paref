a0f299351c1a7fb719745b22943d09ae4759fb2f
Nico Palm <99753436+nicolaipalm@users.noreply.github.com>
2023-06-30 19:37:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Development Status:: 3 - Alpha',
     ],
     python_requires='>=3.6',
     install_requires=[
