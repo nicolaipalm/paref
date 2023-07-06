166b5a5a7892ee0d9a2745fcfc6c8ae0e4e0c060
SebastianMortag <48809025+SebastianMortag@users.noreply.github.com>
2023-07-06 15:08:08 +0200

    added tests for avoid_points input dimension check

--- tests/pareto_reflections/avoid_points_test.py
+++ tests/pareto_reflections/avoid_points_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from paref.pareto_reflections.avoid_points import AvoidPoints
 
@@ -23,3 +24,28 @@ def test_dominating_case():
 
     case = np.ones(2)
     assert ((pareto_reflection(case) == nadir).all())
+
+
+def test_raise_init_value_error_with_input_dimension_mismatch():
+    nadir, epsilon_avoiding_points, epsilon = np.array([3, 7]), np.array([5, 2, 1]), 1
+    with pytest.raises(ValueError,
+                       match=r'.*Nadir and avoiding points need to be 2-dimensional numpy arrays of equal shape!.*'):
+        _ = AvoidPoints(nadir=nadir, epsilon_avoiding_points=epsilon_avoiding_points, epsilon=epsilon)
+
+    nadir, epsilon_avoiding_points, epsilon = np.array([3, 7]), np.array([5, 2]), np.array([5, 2, 1])
+    with pytest.raises(ValueError,
+                       match=r'.*Epsilon must be a Real Number or a numpy array of same shape as nadir!.*'):
+        _ = AvoidPoints(nadir=nadir, epsilon_avoiding_points=epsilon_avoiding_points, epsilon=epsilon)
+
+    nadir, epsilon_avoiding_points, epsilon = np.array([3, 7]), np.array([5, 2]), -1
+    with pytest.raises(ValueError, match=r'.*Epsilon must be positive!.*'):
+        _ = AvoidPoints(nadir=nadir, epsilon_avoiding_points=epsilon_avoiding_points, epsilon=epsilon)
+
+
+def test_raise_value_error_with_differing_input_shapes():
+    nadir, epsilon_avoiding_points, epsilon = np.array([3, 7]), np.array([5, 2]), 1
+    with pytest.raises(ValueError,
+                       match=r'.*Input x must be of dimension 1! Shape of x is *'):
+        pareto_reflection = AvoidPoints(nadir=nadir, epsilon_avoiding_points=epsilon_avoiding_points, epsilon=epsilon)
+        case = np.array([[3, 7], [3, 7]])
+        pareto_reflection(case)
